6329401b87b62d89f011881d01a280436160e09b
anthonette <2015081265@ust-ics.mygbiz.com>
2019-09-17 23:25:49 +0800

    formatted index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Route, NavLink, BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import OrderList from './components/orderlist';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Route, NavLink, BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import OrderList from "./components/orderlist";
+import * as serviceWorker from "./serviceWorker";
 
 const routing = (
-    <Router>
-        <div className="flex-container">
-          <div>
-          <aside className="sidebar">
-              <nav className="nav">
-        <ul>
-          <li>
-            <NavLink exact to="/" activeClassName="active">
-            <i className="fas fa-utensils" />Food</NavLink>
-          </li>
-          <li>
-            <NavLink to="/orders" activeClassName="active">Orders</NavLink>
-          </li>
-        </ul>
-        </nav>
+  <Router>
+    <div className="flex-container">
+      <div>
+        <aside className="sidebar">
+          <nav className="nav">
+            <ul>
+              <li>
+                <NavLink exact to="/" activeClassName="active">
+                  <i className="fas fa-utensils" />
+                  Food
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/orders" activeClassName="active">
+                  Orders
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
         </aside>
-        
-     </div>
-        <Route exact path="/" component={App} />
-        <Route path="/orders" component={OrderList} />
-        </div>
-    </Router>
-  )
+      </div>
+      <Route exact path="/" component={App} />
+      <Route path="/orders" component={OrderList} />
+    </div>
+  </Router>
+);
 
-ReactDOM.render(routing, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
